97f5b5c9dab296d3c3ae08738616d4656f69e8af
Anna Kuszal <anna.kuszal@o2.pl>
2020-05-18 00:52:30 +0200

    Add 2 subpages & referringendpoints

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,12 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  req.user ? res.render('logged') : res.redirect('/no-permission');
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
-
+router.get('/profile', (req, res) => {
+  req.user ? res.render('profile') : res.redirect('/no-permission');
+});
+  
+router.get('/profile/settings', (req, res) => {
+  req.user ? res.render('settings') : res.redirect('/no-permission');
+});
+  
 module.exports = router;
